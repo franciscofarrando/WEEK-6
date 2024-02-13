997921792f33681c31ac93bb19eb9bb7fa9a0edc
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-13 21:35:10 +0100

    Ejercicio 2 listo

---
+++ JS1/E2/main.js
@@ -0,0 +1,11 @@
+let x = parseInt(prompt("Introduce los centrimetros de tu radio"));
+let y = 3.1416
+let area = (y*(x**2));
+2
+//por consola
+console.log ("El area de tu circulo es: ");
+console.log (area);
+
+//por html
+
+document.write("El area de tu circulo es: " + (area) + " cm2");
