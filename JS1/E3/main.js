b9d0daea4970190bbdbbcc863fe6cc566d00af0a
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-14 20:00:21 +0100

    toFixed para sacar los decimales de mas.

--- JS1/E3/main.js
+++ JS1/E3/main.js
@@ -1,7 +1,7 @@
 let x = parseInt(prompt("Introduce los centrimetros de tu radio"));
 let y = 3.1416
-let area = (y*(x**2));
-2
+let area = (y*(x**2)).toFixed(2);
+
 //por consola
 console.log ("El area de tu circulo es: ");
 console.log (area);
