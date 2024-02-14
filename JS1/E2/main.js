0da002e99c54dd297a1b79552faca2a663c444f8
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-14 12:09:37 +0100

    Ejercicio E2

---
+++ JS1/E2/main.js
@@ -0,0 +1,16 @@
+let x = prompt("Cómo te llamás?:");
+let y = prompt("Dónde vivís?:");
+let z = prompt("Qué te gusta (hobbie)?:");
+
+//por consola
+console.log ("Te llamás:");
+console.log (x);
+console.log ("Vivís en:");
+console.log (y);
+console.log ("Te gusta:");
+console.log (z);
+
+
+//por html
+
+document.write("Te llamás " + x + " vivís en " + y + ", y te gusta " + z);
