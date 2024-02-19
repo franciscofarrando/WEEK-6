577f792b87330406430019e42a32667beedbcbe2
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-19 14:27:36 +0100

    terminado. cambios en E3

--- JS CONDICIONALES/E3/main.js
+++ JS CONDICIONALES/E3/main.js
@@ -1,18 +1,11 @@
-let lampara = (prompt("Revise el cable, esta enchufado? 1= NO 2=SI"));
+let lampara = parseInt(prompt("Revise el cable, esta enchufado? 1= NO 2=SI"));
 
 
  if (lampara == 2){
-    let lampara = (prompt("Revise el foco, esta en buen estado? 1= NO 2=SI"));
-    
-} else if (lampara == 1){
-    let lampara = (prompt("Enchufelo! , esto lo soluciono? 1= NO 2=SI"));
-1
-} else if (lampara != 2){
-  document.write("Cambie el foco, esto lo soluciono? 1= NO 2=SI");
-//} else if (lampara == 1){
- //   let lampara = (prompt("Cambie el foco, esto lo soluciono? 1= NO 2=SI"));
-///} else if (lampara == 2){
-//document.write("Solucionado era el foco!");
-}else {
-    document.write("Considere reemplazar la lampara");
-}
+    let foco = parseInt(prompt("Revise el foco, esta en buen estado? 1= NO 2=SI"));
+    if (foco == 2){
+        document.write("Considere cambiar la Lampara");
+    } else { document.write("Cambie el foco!")}
+ } else {
+    document.write("Enchufe la Lampara")
+ }
